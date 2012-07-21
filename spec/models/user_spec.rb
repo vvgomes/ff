5d122ec74b7485362324c4e950b8df03a2435971
Vinicius Gomes <vvgomess@gmail.com>
2012-07-21 11:11:55 -0300

    adding name validation to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
-require 'spec_helper'
-
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not be valid without name' do
+    User.new.should_not be_valid
+  end
+
+  it 'should be valid with a name' do
+    User.new(:name => 'Jairo').should be_valid
+  end
 end
