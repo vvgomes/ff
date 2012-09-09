13267de2025403ae663e62ce4c64887274c79ec0
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-09-09 02:13:27 -0400

    accomplishment create redirects to index

--- app/controllers/accomplishments_controller.rb
+++ app/controllers/accomplishments_controller.rb
@@ -5,41 +5,14 @@ class AccomplishmentsController < ApplicationController
     respond_to { |format| format.html }
   end
 
-  def show
-    @accomplishment = Accomplishment.find(params[:id])
-    respond_to { |format| format.html }
-  end
-
-  def new
-    @accomplishment = Accomplishment.new
-    respond_to { |format| format.html }
-  end
-
   def create
     @accomplishment = Accomplishment.new(params[:accomplishment])
     respond_to do |format|
       if @accomplishment.save
-        format.html { redirect_to @accomplishment, notice: 'Accomplishment was successfully created.' }
+        format.html { redirect_to accomplishments_path, notice: 'Accomplishment reported!' }
       else
         format.html { render action: 'new' }
       end
     end
-  end
-
-  def update
-    @accomplishment = Accomplishment.find(params[:id])
-    respond_to do |format|
-      if @accomplishment.update_attributes(params[:accomplishment])
-        format.html { redirect_to @accomplishment, notice: 'Accomplishment was successfully updated.' }
-      else
-        format.html { render action: 'edit' }
-      end
-    end
-  end
-
-  def destroy
-    @accomplishment = Accomplishment.find(params[:id])
-    @accomplishment.destroy
-    respond_to { |format| format.html { redirect_to accomplishments_url } }
-  end
+  end  
 end
--- spec/controllers/accomplishments_controller_spec.rb
+++ spec/controllers/accomplishments_controller_spec.rb
@@ -17,4 +17,20 @@ describe AccomplishmentsController do
     it { should render_template :index }
     it { should_not set_the_flash }
   end
+
+  describe '#create' do
+    let(:poster_id) { user.id }
+    let(:receiver_id) { create(:user).id }
+    let(:attrs) do
+      attributes_for(:accomplishment).merge({
+        poster_id: poster_id, receiver_id: receiver_id
+      })
+    end
+
+    before { post :create, accomplishment: attrs }
+
+    it { should respond_with(302) }
+    it { should redirect_to accomplishments_path }
+    it { should set_the_flash.to('Accomplishment reported!') }
+  end
 end
