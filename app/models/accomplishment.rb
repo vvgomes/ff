8358d786dc1abc0b5c18068ce0479888649dfd4a
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-09-10 18:54:21 -0400

    you cannot post accomplishments to yourself

--- app/models/accomplishment.rb
+++ app/models/accomplishment.rb
@@ -3,4 +3,12 @@ class Accomplishment < ActiveRecord::Base
   belongs_to :receiver, :class_name => 'User'
   validates_presence_of :description, :poster, :receiver
   attr_accessible :description, :poster, :receiver, :poster_id, :receiver_id
+
+  validate :poster_cannot_be_receiver
+ 
+  def poster_cannot_be_receiver
+    if poster == receiver
+      errors.add(:receiver, "can't be yourself")
+    end
+  end
 end
--- spec/models/accomplishment_spec.rb
+++ spec/models/accomplishment_spec.rb
@@ -2,4 +2,10 @@ describe Accomplishment do
   it { should validate_presence_of :description }
   it { should validate_presence_of :receiver }
   it { should validate_presence_of :poster }
+
+  context 'when the poster and the receiver are the same person' do
+    let(:cheater) { build :user }
+    subject { build :accomplishment, poster: cheater, receiver: cheater, description: 'oh' }
+    it { should_not be_valid }
+  end
 end
