844acd10161da2e4df74f5e31150c5760369d985
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-09-24 03:40:52 -0400

    adds suggestion routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ FF::Application.routes.draw do
   devise_for :users
 
   resources :accomplishments
+  resources :suggestions
   resources :users
 
   get '/:username', :to => 'accomplishments#index', :as => :user
