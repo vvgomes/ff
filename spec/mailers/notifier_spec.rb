7e356320b7974d7770fe9ef9ba65ee52ef96ed22
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-10-05 15:04:27 -0400

    using user`s email as sender

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -6,7 +6,7 @@ describe Notifier do
     subject { Notifier.accomplishment(acc) }
     let(:acc) { build :accomplishment, :poster => tc, :receiver => leo }
     its(:subject) { should == 'Accomplishment reported by tc' }
-    its(:from) { should == ['fastfeedback@gmail.com'] }
+    its(:from) { should == ['tc@thoughtworks.com'] }
     its(:to) { should == ['leo@thoughtworks.com'] }
   end
 
@@ -14,7 +14,7 @@ describe Notifier do
     subject { Notifier.suggestion(sug) }
     let(:sug) { build :suggestion, :sender => tc, :receiver => leo }
     its(:subject) { should == 'Improvements suggested by tc' }
-    its(:from) { should == ['fastfeedback@gmail.com'] }
+    its(:from) { should == ['tc@thoughtworks.com'] }
     its(:to) { should == ['leo@thoughtworks.com'] }
   end
 
