b9692a8e1155ba9f9e15b03d8370308626e7ab7d
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-10-11 16:29:11 -0400

    thumbs ups controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FF::Application.routes.draw do
   resources :accomplishments
   resources :suggestions
+  resources :thumbs_ups
   devise_for :users
   get '/:username', :to => 'users#index', :as => 'user'
   root to: 'application#index'
