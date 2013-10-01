1ea6a7162e00fa01c1299064dc11aaeaab55bfe0
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-09-30 21:45:18 -0300

    simplifies suggestion controller

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,5 +1,4 @@
 class SuggestionsController < ApplicationController
-
   def create
     raw = params[:suggestion]
     description = raw[:description]
@@ -12,34 +11,4 @@ class SuggestionsController < ApplicationController
       redirect_to user_path(receiver.username)
     end
   end
-
-  def update
-    if (@suggestion = try_suggestion params[:id])
-     @suggestion.approve!
-     @suggestion.save
-      Notifier.approval(@suggestion).deliver
-     redirect_to '/', notice: 'Suggestion approved!'
-    else
-      redirect_to '/'
-    end
-  end
-
-  def edit
-    if (@suggestion = try_suggestion params[:id])
-      @user = current_user
-      @accomplishments = Accomplishment.latest.paginate(:page => params[:page])
-      @accomplishment = Accomplishment.new
-      render :edit
-    else
-      redirect_to '/'
-    end
-  end
-
-  private
-
-  def try_suggestion id
-    s = Suggestion.find_by_id id
-    current_user.able_to_approve?(s) ? s : nil
-  end
-
 end
