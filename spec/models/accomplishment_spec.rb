d5fcf202063cb14ed489f2783fd763f05931f4eb
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-10-01 10:47:10 -0300

    accomplishment has many plus_ones

--- spec/models/accomplishment_spec.rb
+++ spec/models/accomplishment_spec.rb
@@ -1,6 +1,7 @@
 describe Accomplishment do
   it { should belong_to :poster }
   it { should belong_to :receiver }
+  it { should have_many :plus_ones }
 
   it { should allow_mass_assignment_of :description }
   it { should allow_mass_assignment_of :receiver }
