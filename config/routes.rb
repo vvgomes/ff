59b7fd4f806e1dde2c19e974923f8aa9325c1f90
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-10-12 12:52:44 -0300

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 FF::Application.routes.draw do
+  devise_for :users
   resources :accomplishments do
     resources :plus_ones
   end
   resources :suggestions
-  devise_for :users
   get '/:username', :to => 'users#index', :as => 'user'
   root to: 'application#index'
 end
