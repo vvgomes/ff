11a382d1848a5fee0eb2452bd30fd4505abcc954
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-10-24 20:40:20 -0200

    +1 givers only with css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,14 +26,6 @@ $(document).ready(function() {
     name: 'username', local: usersInput.val().split(',')
   });
 
-  $('.plus-one-givers-link').on('mouseover', function() {
-    $('.plus-one-givers[data-acc='+$(this).data('acc')+']').toggle();
-  });
-
-  $('.plus-one-givers').on('mouseleave', function() {
-    $(this).hide();
-  });
-
   buildAccomplishmentChart();
   buildTagsChart();
   buildGivenTagsChart();
